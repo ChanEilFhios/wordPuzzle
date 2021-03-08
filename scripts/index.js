20932f59bfa96c54f1f87c45d35affb300a60329
Brendan O'Brien <brendan.obrien@kroger.com>
2021-03-08 12:23:20 -0500

    fix indentation

--- scripts/index.js
+++ scripts/index.js
@@ -5,43 +5,43 @@ const collectWords = words => words.map(wordEntry => ({word: wordEntry.word, lev
 const collectSubWords = subWords => subWords.flatMap(key => collectWords(allWords[key].words))
 
 function setupGame() {
-    const answerWord = getRandomFromArray(nines)
-    const answerLetters = answerWord.split('')
-    const requiredLetterIndex = getRandomIndexFromArray(answerLetters)
+  const answerWord = getRandomFromArray(nines)
+  const answerLetters = answerWord.split('')
+  const requiredLetterIndex = getRandomIndexFromArray(answerLetters)
   
-    return {
-        answerWord,
-        requiredLetter: answerLetters[requiredLetterIndex],
-        optionalLetters: answerLetters.filter((a, idx) => idx !== requiredLetterIndex),
-        words: collectSubWords(allWords[sortCharsInStr(answerWord)].subwords)
-                .sort((a, b) => (a.word < b.word) ? -1 : 1),
-        guesses: [],
-        gameOver: false
-    }
+  return {
+    answerWord,
+    requiredLetter: answerLetters[requiredLetterIndex],
+    optionalLetters: answerLetters.filter((a, idx) => idx !== requiredLetterIndex),
+    words: collectSubWords(allWords[sortCharsInStr(answerWord)].subwords)
+        .sort((a, b) => (a.word < b.word) ? -1 : 1),
+    guesses: [],
+    gameOver: false
+  }
 }
 
 const attachRenderer = (elementSelector, renderer) => {
-    const element = document.body.querySelector(elementSelector)
-    return (state => {
-        renderer(state, element)
-    })
+  const element = document.body.querySelector(elementSelector)
+  return (state => {
+    renderer(state, element)
+  })
 }
 
 window.state = state
 
 state.registerAction("new-word", state => setupGame())
 state.registerRenderer(attachRenderer("flower-layout", (state, element) => {
-    const optionals = [...state.optionalLetters]
-    for (let i = 0; i < element.children.length; i++) {
-        if (i === 0) {
-            element.children[0].textContent = state.requiredLetter.toUpperCase()
-        } else {
-            const rndIdx = getRandomIndexFromArray(optionals)
+  const optionals = [...state.optionalLetters]
+  for (let i = 0; i < element.children.length; i++) {
+    if (i === 0) {
+      element.children[0].textContent = state.requiredLetter.toUpperCase()
+    } else {
+      const rndIdx = getRandomIndexFromArray(optionals)
 
-            element.children[i].textContent = optionals[rndIdx].toUpperCase()
-            optionals.splice(rndIdx, 1)
-        }
+      element.children[i].textContent = optionals[rndIdx].toUpperCase()
+      optionals.splice(rndIdx, 1)
     }
+  }
 }))
 state.initializeState(setupGame())
 state.dumpState()
